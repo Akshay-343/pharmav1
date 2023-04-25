f54d78d12eef67b911cd64088950aa5bd17a856b
akshay343 <127043464+Akshay-343@users.noreply.github.com>
2023-04-26 00:41:01 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'static')
 STATICFILES_DIRS = [STATIC_DIR, MEDIA_ROOT]
 STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
 LOGIN_REDIRECT_URL = '/afterlogin'
